b4b0bc3d5944aad8805ffcb79315ec29709d4614
Alie <aboondooley@gmail.com>
2020-10-05 23:00:26 -0700

    Header file complete and started implementing the cpp.

--- Rating.cpp
+++ Rating.cpp
@@ -3,3 +3,56 @@
 //
 
 #include "Rating.h"
+
+
+Rating::Rating() {
+    memberCapacity = INITIAL_CAPACITY;
+    bookCapacity = INITIAL_CAPACITY;
+    ratingArray = new int*[INITIAL_CAPACITY];
+    for (int i = 0; i <INITIAL_CAPACITY; i++)
+        ratingArray[i] = new int[INITIAL_CAPACITY];
+}
+
+Rating::Rating(const Rating &other) {
+
+}
+
+Rating &Rating::operator=(const Rating &rhs) {
+    return <#initializer#>;
+}
+
+Rating::~Rating() {
+
+}
+
+bool Rating::addRating(int memberId, int bookId, int rating) {
+    return false;
+}
+
+int Rating::getRating(int memberId) const {
+    return 0;
+}
+
+int Rating::mostSimilar(int memberId) const {
+    return 0;
+}
+
+void Rating::addMember(int memberId) {
+
+}
+
+void Rating::addBook(int bookId) {
+
+}
+
+void Rating::resizeCols(int newCapac) {
+
+}
+
+void Rating::resizeRows(int newCapac) {
+
+}
+
+void Rating::clear() {
+
+}
--- Rating.h
+++ Rating.h
@@ -4,7 +4,9 @@
 
 #ifndef PROJECT2_RECOMMENDATIONS_RATING_H
 #define PROJECT2_RECOMMENDATIONS_RATING_H
+
 #include <iostream>
+
 
 /**
  * The Rating class holds a 2D array of the ratings for all members for all
@@ -15,29 +17,88 @@
  */
 class Rating {
 public:
-    /**
-     * Get recommendations for the member with the given account number
-     * The 5 best recommendations will be printed out to the console for the
-     * client.
-     * @param accountNum = the account number of the member for whom you
-     * would like to get recommendations for (int)
-     */
-    void getRecs(int accountNum);
 
     /**
-     * Get recommendations for the member with the given members name.
-     * Overloaded method that takes a name instead of account number.
-     * The 5 best recommendations will be printed out to the console for the
-     * client.
-     * @param name = the name of the member for whom you would like to get
-     * recommendations for (string)
+     * Constructor - no arg
      */
-    void getRecs(std::string name);
+    Rating();
+
+    /**
+     * Copy constructor
+     * @param other Rating object to copy
+     */
+    Rating(const Rating &other);
+
+    /**
+     * Assignment operator
+     * @param rhs Rating object to copy
+     * @return myself, new and improved
+     */
+    Rating &operator=(const Rating &rhs);
+
+    /**
+     * Dtor
+     */
+    ~Rating();
+
+    /**
+     * Add a rating for a particular member for a particular book
+     * @param memberId account number of the rating member
+     * @param bookId bookId of the book to rate
+     * @param rating value of rating
+     * @return true if the rating was successful, false if the member or
+     * book doesn't exist
+     */
+    bool addRating(int memberId, int bookId, int rating);
+
+
+    /**
+     * Gets the rating for the member and book
+     * @param memberId member account number
+     * @param bookId the bookId of the rating to look up
+     * @return the rating of the book in question
+     */
+     int getRating(int memberId) const;
+
+     /**
+      * Get all of the ratings for a particular member
+      * @param memberId account number of the member to print for
+      */
+
+    /**
+     * Get recommendations for the member with the given account number
+     * The books that with the closest score loved and liked will be recommended
+     * @param memberId the account number of the member for whom you
+     * would like to get recommendations for (int)
+     * @return the account number of the member with the most similar ratings
+     */
+    int mostSimilar(int memberId) const;
+
+    /**
+     * Adds a member to the rating 2D array - adds a row for a new member
+     * Sets all of their ratings for that member to zero
+     * @param memberId the account number of the member to add
+     */
+    void addMember(int memberId);
+
+    /**
+    * Adds a book to the rating 2D array - adds a column for a new book
+    * Sets all of their ratings for that book to zero
+    * @param bookId the bookId of the book to add
+    */
+    void addBook(int bookId);
+
+
 private:
-    int COLS; // number of books/columns
-    int ROWS; // number of members/rows
-    int **ratings; // 2D array of books (columns) and members (rows) filled
-    // with ratings
+    const static int INITIAL_CAPACITY = 30;
+    const static int RATE_ZERO = 0;
+    int **ratingArray;
+    int memberCapacity;
+    int bookCapacity;
+
+    void resizeCols(int newCapac);
+    void resizeRows(int newCapac);
+    void clear();
 };
 
 
