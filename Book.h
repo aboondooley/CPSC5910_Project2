25d9e9bd827d62ead872c1f2e64f4e0ce7d30d17
Alie <aboondooley@gmail.com>
2020-10-05 17:58:21 -0700

    Changing so it represents the state of my assignment when I submitted the ICE.

--- Book.h
+++ Book.h
@@ -7,90 +7,59 @@
 #include <iostream>
 
 /**
- * The Book class is a collection of books, or a library. Blank books can be
- * constructed using the ctor. BookLog info can be added to the collection ,
- * and copies of existing books can be made using the assignment operator or
- * copy ctor. THe user can look up the BookId with the ISBN. You can check
- * the size of the library, print out a book and the capacity of the library
- * will increase automatically.
- * ISBN is assigned to new books by incrementing the ISBN
- * number each time, which is one greater than the bookId.
+ * The Book class is a collection of books, a library, if you will. Members
+ * can log in, add a new book to the library, and also rate a book that they
+ * have read.
+ * It also assigns ISBN to new books by incrementing the ISBN
+ * number each time.
  */
 class Book {
 public:
-    static const int I_CAPACITY = 60; // initial Book capacity
-    /*
-     * No arg constructor
-     */
-    Book();
-    /*
-     * Constructor with author, title and year set by user
-     */
-
-    /*
-     * Copy constructor
-     */
-    Book(const Book &other);
-
-    /*
-     * Destructor
-     */
-    ~Book();
-
-    /*
-     * Assignment operator overload
-     */
-    Book &operator=(const Book &rhs);
-
     /**
      * Adds a new book to the library. All three of the fields are required
      * since the librarian is required to keep track of all four fields.
-     * @param isbn = the ISBN associated with the book
      * @param author = the author of the book (string)
      * @param title = the title of the book (string)
      * @param year = year that the book was written (int)
-     * @return returns the BookID
+     * @return 0 to signal signal success and -1 to signal that the book
+     * already exists
      */
-    int addNewBook(const int isbn, const std::string author,
-                   const std::string title, const std::string year);
-
+    int addNewBook(std::string author, std::string title, int year);
 
     /**
-     * Looks up the ISBN of the book based on its BookID
-     * I changed the name because it makes more sense to me that we are
-     * looking up the BookId.
-     * @param isbn = The ISBN associated with the book in question
-     * @return BookID which is the unique ID for the book
+     * Allows the member to rate a particular book. The member must know the
+     * name of the member as well as the author and book name, and year.
+     * @param member = the name of the member who is rating the book (string)
+     * @param author = name of the author of the book (string)
+     * @param title = title of the book (string)
+     * @param year = year that the book was written (int)
+     * @return 0 if the rating was successful and -1 if the book or member
+     * do not exist
      */
-    int lookUpBookId(int isbn) const;
-
+    int rateBook(std::string member, std::string author, std::string title, int
+    year);
     /**
-     * Sees how many books are in the "library" of books.
-     * @return number of books in the book collection
+     * Again, allows the member to rate a particular book. The member must know
+     * the account number of the member as well as the author and book name,
+     * and year.
+     * This an overload method for rating books when there is a member of the
+     * same name
+     * @param accountNum = the account number of the member who is rating (int)
+     * @param author = name of the author of the book (string)
+     * @param title = title of the book (string)
+     * @param year = year that the book was written (int)
+     * @return 0 if the rating was successful and -1 if the book or member
+     * do not exist
      */
-    int size() const;
-
-
-    void printBook(int bookID) const;
+    int rateBook(int accountNum, std::string author, std::string title, int
+    year);
 
 private:
-    static const int UH_OH = -1;
+
     int idCounter; // keeps track of what the used ids are (increments)
-    int capacity; // keeps track of the capacity of the current book array
-    struct BookLog {
-        int isbn;
-        std::string author;
-        std::string title;
-        std::string year;
-    };
-    BookLog *library; // Ann array of book instances (book logs)
-
-    /**
-     * Resizes (doubles) the list if the I_CAPACITY is exceeded.
-     */
-    void resize();
-
-
+// I am not sure how I will store the book class yet.
+// I don't think I can use a 2D array because some cells will have strings
+// and some will have ints
 };
 
 
